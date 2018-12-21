9e822d37da763173033b5c129f9834b24bd9d3f7
CatchZeng <891793848@qq.com>
2018-12-21 10:49:04 +0800

    feat: add stylus support

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,25 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.vue$/,
+        test: /\.vue$/,
         loader: "vue-loader"
       },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.styl$/,
+        use: ["style-loader", "css-loader", "stylus-loader"]
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 1024, name: "[name].[ext]" }
+          }
+        ]
       }
     ]
   },
