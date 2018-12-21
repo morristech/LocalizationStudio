757311cb5639bdb1c5e78dcc607f4a908adca36a
CatchZeng <891793848@qq.com>
2018-12-21 11:23:28 +0800

    feat: webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path");
+const webpack = require("webpack");
+const HTMLPlugin = require("html-webpack-plugin");
 
 //https://segmentfault.com/q/1010000014770071?sort=created
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
-module.exports = {
+const isDev = process.env.NODE_ENV === "development";
+
+const config = {
+  target: "web",
   entry: path.join(__dirname, "src/index.js"),
   output: {
     filename: "bundle.js",
@@ -34,5 +39,32 @@ module.exports = {
       }
     ]
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin()
+  ]
 };
+
+if (isDev) {
+  config.devtool = "#cheap-module-eval-source-map";
+  config.devServer = {
+    port: 8000,
+    host: "0.0.0.0",
+    overlay: {
+      error: true
+    },
+    open: true,
+    hot: true
+  };
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  );
+}
+
+module.exports = config;
