79f906498d1f5e8fe71e4dbbef930faf66252905
CatchZeng <891793848@qq.com>
2019-01-02 16:58:31 +0800

    feat: add electron

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile("index.html");
+
+  win.webContents.openDevTools();
+
+  win.on("closed", () => {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+  // 通常在应用程序中重新创建一个窗口。
+  if (win === null) {
+    createWindow();
+  }
+});
